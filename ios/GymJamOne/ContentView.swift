3601d0303fd958ae2269d357bdacbbedc7f5f28e
rahulmadduluri <rahulmadduluri@gmail.com>
2019-09-30 13:50:20 -0700

    start git repo

---
+++ ios/GymJamOne/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GymJamOne
+//
+//  Created by Rahul Madduluri on 9/21/19.
+//  Copyright © 2019 GymJam. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
