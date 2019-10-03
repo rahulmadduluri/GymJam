c6dbba78f61f2ee233eae8eba5fe1b0ec88f073e
rahulmadduluri <rahulmadduluri@gmail.com>
2019-10-02 17:58:13 -0700

    pause instructor player when video is dismissed

--- ios/GymJamOne/WorkoutInstructorView.swift
+++ ios/GymJamOne/WorkoutInstructorView.swift
@@ -11,36 +11,48 @@ import AVKit
 
 struct WorkoutInstructorView: UIViewRepresentable {
 
-  func updateUIView(_ uiView: UIView, context: UIViewRepresentableContext<WorkoutInstructorView>) {
-  }
-  func makeUIView(context: Context) -> UIView {
-    // create PlayerUIView
-    return WorkoutInstructorUIView(frame: .zero)
-  }
+    let player: AVPlayer
+
+    func updateUIView(_ uiView: UIView, context: UIViewRepresentableContext<WorkoutInstructorView>) {
+    }
+    func makeUIView(context: Context) -> UIView {
+        return WorkoutInstructorUIView(player: player)
+    }
 }
 
 class WorkoutInstructorUIView: UIView {
 
   private let playerLayer = AVPlayerLayer()
-
-  override init(frame: CGRect) {
-    super.init(frame: frame)
-
-    guard let path = Bundle.main.path(forResource: "hiit_example_sample", ofType:"mp4") else {
-        debugPrint("hiit_example_sample.mp4 not found")
-        return
+    
+    init(player: AVPlayer) {
+        super.init(frame: .zero)
+        
+        playerLayer.player = player
+        layer.addSublayer(playerLayer)
+        
+        player.play()
     }
-    let player = AVPlayer(url: URL(fileURLWithPath: path))
-    player.play()
-
-    playerLayer.player = player
-    layer.addSublayer(playerLayer)
-  }
+//
+//  override init(frame: CGRect) {
+//    super.init(frame: frame)
+//
+//    guard let path = Bundle.main.path(forResource: "hiit_example_sample", ofType:"mp4") else {
+//        debugPrint("hiit_example_sample.mp4 not found")
+//        return
+//    }
+//    let player = AVPlayer(url: URL(fileURLWithPath: path))
+//    player.play()
+//
+//    playerLayer.player = player
+//    layer.addSublayer(playerLayer)
+//  }
   required init?(coder: NSCoder) {
    fatalError("init(coder:) has not been implemented")
   }
+    
   override func layoutSubviews() {
     super.layoutSubviews()
     playerLayer.frame = bounds
   }
+    
 }
--- ios/GymJamOne/WorkoutPlayer.swift
+++ ios/GymJamOne/WorkoutPlayer.swift
@@ -7,12 +7,23 @@
 //
 
 import SwiftUI
+import AVKit
 
 struct WorkoutPlayer: View {
+    
+    let player: AVPlayer
+    
+    init() {
+        let instructorVideoPath = Bundle.main.path(forResource: "hiit_example_sample", ofType:"mp4")!
+        player = AVPlayer(url: URL(fileURLWithPath: instructorVideoPath))
+    }
         
     var body: some View {
         HStack {
-            WorkoutInstructorView()
+            WorkoutInstructorView(player: player)
+                .onDisappear() {
+                    self.player.pause()
+                }
             WorkoutCameraView()
             WorkoutStatsView()
         }
