a88ddb8b8343abb1cc07df5aa813f10b5a6f7e76
rahulmadduluri <rahulmadduluri@gmail.com>
2019-10-14 22:38:28 -0700

    robot moves with you

--- ios/GymJamOne/Workout/WorkoutCameraViewAR.swift
+++ ios/GymJamOne/Workout/WorkoutCameraViewAR.swift
@@ -9,6 +9,7 @@
 import SwiftUI
 import RealityKit
 import ARKit
+import Combine
 
 struct WorkoutCameraViewAR: View {
     var body: some View {
@@ -30,7 +31,7 @@ struct RealityIntegratedViewController: UIViewControllerRepresentable {
     }
 }
 
-class RealityViewController: UIViewController {
+class RealityViewController: UIViewController, ARSessionDelegate {
     
 //    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
 //        return .portrait
@@ -39,13 +40,25 @@ class RealityViewController: UIViewController {
 //    override open var shouldAutorotate: Bool {
 //        return false
 //    }
+    
+    // The 3D character to display.
+    var character: BodyTrackedEntity?
+    let characterOffset: SIMD3<Float> = [0, 0, 0] // Offset the character by one meter to the left
+    let characterAnchor = AnchorEntity()
             
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Add a background color a placerholder
         //Comment out below this when previewing!
         // Create a new ARView
         let arView = ARView(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.width/UIScreen.main.bounds.height * UIScreen.main.bounds.width))
+        arView.session.delegate = self
+        
+        guard ARBodyTrackingConfiguration.isSupported else {
+            fatalError("This feature is only supported on devices with an A12 chip")
+        }
+
         // Add the ARView to the view
         view.addSubview(arView)
         // Load the "Box" scene from the "Experience" Reality File
@@ -53,19 +66,50 @@ class RealityViewController: UIViewController {
         // Add the box anchor to the scene
 //        arView.scene.anchors.append(boxAnchor)
         
-        if ARBodyTrackingConfiguration.isSupported {
-            let configuration = ARBodyTrackingConfiguration()
-            arView.session.run(configuration)
-        }
-        let bodyAnchor = AnchorEntity(.body)
-        arView.scene.anchors.append(bodyAnchor)
+        let configuration = ARBodyTrackingConfiguration()
+        arView.session.run(configuration)
+//        let bodyAnchor = AnchorEntity(.body)
+        arView.scene.anchors.append(characterAnchor)
         
-        Entity.loadBodyTrackedAsync(named: "robot").sink(receiveCompletion: { (error) in
-            //error
-        }) { (character) in
-            bodyAnchor.addChild(character)
-        }
-        
+        // Asynchronously load the 3D character.
+        var cancellable: AnyCancellable? = nil
+        cancellable = Entity.loadBodyTrackedAsync(named: "character/robot").sink(
+            receiveCompletion: { completion in
+                if case let .failure(error) = completion {
+                    print("Error: Unable to load model: \(error.localizedDescription)")
+                }
+                cancellable?.cancel()
+        }, receiveValue: { (character: Entity) in
+            if let character = character as? BodyTrackedEntity {
+                // Scale the character to human size
+                character.scale = [1.0, 1.0, 1.0]
+                self.character = character
+                cancellable?.cancel()
+            } else {
+                print("Error: Unable to load model as BodyTrackedEntity")
+            }
+        })
+
+    }
+    
+     func session(_ session: ARSession, didUpdate anchors: [ARAnchor]) {
+         for anchor in anchors {
+             guard let bodyAnchor = anchor as? ARBodyAnchor else { continue }
+             
+             // Update the position of the character anchor's position.
+             let bodyPosition = simd_make_float3(bodyAnchor.transform.columns.3)
+             characterAnchor.position = bodyPosition + characterOffset
+             // Also copy over the rotation of the body anchor, because the skeleton's pose
+             // in the world is relative to the body anchor's rotation.
+             characterAnchor.orientation = Transform(matrix: bodyAnchor.transform).rotation
+    
+             if let character = character, character.parent == nil {
+                 // Attach the character to its anchor as soon as
+                 // 1. the body anchor was detected and
+                 // 2. the character was loaded.
+                 characterAnchor.addChild(character)
+             }
+         }
     }
     
 }
